caa78e5fef564e6004f392a47696e4791d2f7fc8
Renon bart <b.renon@mail.com>
2026-03-17 14:07:49 +0100

    del fihcierAjoutez des fichiers projet.

---
+++ Classe/Personne.cs
@@ -0,0 +1,18 @@
+﻿namespace GestionProjet.Classe
+{
+    public class Personne
+    {
+        public string nom { get; set; }
+
+        public string prenom { get; set; }
+
+        public DateTime dateNaiss { get; set; }
+
+        public Personne(string nom, string prenom)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            dateNaiss = DateTime.Now;
+        }
+    }
+}
