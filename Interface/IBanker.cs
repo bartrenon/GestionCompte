82516142e85dd8a709366da5ce4a33f38848536e
Renon bart <b.renon@mail.com>
2026-03-20 11:19:49 +0100

    factorisation du code

--- Interface/IBanker.cs
+++ Interface/IBanker.cs
@@ -2,16 +2,12 @@
 
 namespace GestionCompte.Interface
 {
-    interface IBanker
+    interface IBanker : ICustomer
     {
-        public decimal solde { get;}
-
         public string numero { get;}
 
         public Personne titulaire { get;}
 
-        public bool Depot(decimal montant);
-        public bool Retrait(decimal montant);
         public void AppliquerInteret();
 
     }
